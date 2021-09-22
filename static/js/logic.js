c4fee5e1372cdae5392e21a8910208a431a63ebd
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-22 01:02:17 -0700

    Added API key

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,3 +5,29 @@ d3.json(URL, function(data) {
     console.log(data)
     createFeatures(data.features);
 });
+function createFeatures(earthquakeData) {
+    function onEachLayer(feature) {
+        return new L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+          radius: circleSize(feature.properties.mag),
+          fillOpacity: 0.8,
+          color: getColor(feature.properties.mag),
+          fillColor: getColor(feature.properties.mag)
+        });
+      }
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+  
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  }
+function createMap(earthquakes) {
